292fcd5506866175098c2f1586daa9833d0c3be4
ganesha87 <ganeshvarma.cse@gmail.com>
2023-01-26 00:05:12 +0530

    mod-fox-atmpt-3

--- modules/group-rules/main.tf
+++ modules/group-rules/main.tf
@@ -7,10 +7,13 @@ terraform {
   }
 }
 resource "okta_group_rule" "this" {
-  for_each         = var.group_rules
+  for_each         = local.group_rules_map
   name              = each.name
   status            = "ACTIVE"
   group_assignments = each.group_assignments
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = each.expression_value
+}
+locals{
+  group_rules_map  = { for gr in var.group_rules : gr.name => gr }
 }
